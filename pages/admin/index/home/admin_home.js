0e0151aa486e62495b6b6508b057836a467df544
moshangongzi <2061935864@qq.com>
2022-06-17 16:13:19 +0800

    加入了一些js文件，完成了后台管理的index页面

--- pages/admin/index/home/admin_home.js
+++ pages/admin/index/home/admin_home.js
@@ -1,66 +1,100 @@
-// pages/admin/index/home/admin_home.js
+const AdminBiz = require('../../../../biz/admin_biz');
+const pageHelper = require('../../../../helper/page_helper.js');
+
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		formContent: [{
+			type: 'text',
+			val: '',
+		}]
+	},
 
-    },
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: async function (options) {
+		// if (!AdminBiz.isAdmin(this)) return;
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
+		let parent = pageHelper.getPrevPage(2);
+		if (!parent) return;
 
-    },
+		let formContent = parent.data.formContent;
+		if (formContent && formContent.length > 0)
+			this.setData({
+				formContent
+			});
+	},
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
 
-    },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
 
-    },
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
+	},
 
-    },
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {},
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {
 
-    },
+	},
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {
 
-    },
+	},
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: async function () {
 
-    },
+	},
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
+	model: function (e) {
+		pageHelper.model(this, e);
+	},
 
-    }
+	bindSaveTap: function (e) {
+		let parent = pageHelper.getPrevPage(2);
+		if (!parent) return;
+		parent.setData({
+			formContent: this.data.formContent
+		});
+	},
+
+	url: function (e) {
+		pageHelper.url(e, this);
+	},
+
+	bindSaveTap: function (e) {
+		let formContent = this.selectComponent("#contentEditor").getNodeList();
+
+		let parent = pageHelper.getPrevPage(2);
+		if (!parent) return;
+
+		parent.setData({
+			formContent
+		}, () => {
+			parent._setContentDesc();
+		});
+
+		wx.navigateBack({
+			delta: 0,
+		});
+	}
 })
