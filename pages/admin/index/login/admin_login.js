3ecedc3bf5bfdf9f0306594945448f5065256613
moshangongzi <2061935864@qq.com>
2022-06-17 16:32:52 +0800

    后台登录页面的样式

--- pages/admin/index/login/admin_login.js
+++ pages/admin/index/login/admin_login.js
@@ -1,66 +1,62 @@
-// pages/admin/index/login/admin_login.js
+const AdminBiz = require('../../../../biz/admin_biz.js'); 
+const pageHelper = require('../../../../helper/page_helper.js'); 
+const PassportBiz = require('../../../../biz/passport_biz.js');
+
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+	/**
+	 * 页面的初始数据
+	 */
+	data: {
+		name: '',
+		pwd: '',
+	},
 
-    },
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {
+		AdminBiz.clearAdminToken();
+	},
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {
 
-    },
+	},
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {},
 
-    },
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+	},
 
-    },
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
+	},
 
-    },
+	url: function (e) {
+		pageHelper.url(e, this);
+	},
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
+	bindBackTap: function (e) {
+		wx.reLaunch({
+			url: pageHelper.fmtURLByPID('/pages/my/index/my_index'),
+		});
+	},
 
-    },
+	bindLoginTap: async function (e) {
+		return PassportBiz.adminLogin(this.data.name, this.data.pwd, this);
+	}
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
 })
