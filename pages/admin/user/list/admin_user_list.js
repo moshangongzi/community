518b2755e0dc7be2d642ac45e9ff28efbfc28442
Amy <2503360709@qq.com>
2022-06-20 09:25:41 +0800

    用户管理的界面

--- pages/admin/user/list/admin_user_list.js
+++ pages/admin/user/list/admin_user_list.js
@@ -1,66 +1,107 @@
-// pages/admin/user/list/admin_user_list.js
+const AdminBiz = require('../../../../biz/admin_biz.js');
+const pageHelper = require('../../../../helper/page_helper.js');
+const cloudHelper = require('../../../../helper/cloud_helper.js');
+
 Page({
 
-    /**
-     * 页面的初始数据
-     */
-    data: {
+	onLoad: async function (options) {
+		if (!AdminBiz.isAdmin(this)) return;
 
-    },
+		//设置搜索菜单
+		await this._getSearchMenu();
+	},
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad: function (options) {
+	url: async function (e) {
+		pageHelper.url(e, this);
+	},
 
-    },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+	bindCommListCmpt: function (e) {
+		pageHelper.commListListener(this, e);
+	},
 
-    },
+	bindDelTap: async function (e) {
+		if (!AdminBiz.isAdmin(this)) return;
+		let id = pageHelper.dataset(e, 'id');
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+		let params = {
+			id
+		}
 
-    },
+		let callback = async () => {
+			try {
+				let opts = {
+					title: '删除中'
+				}
+				await cloudHelper.callCloudSumbit('admin/user_del', params, opts).then(res => {
+					
+					pageHelper.delListNode(id, this.data.dataList.list, 'USER_MINI_OPENID');
+					this.data.dataList.total--;
+					this.setData({
+						dataList: this.data.dataList
+					});
+					pageHelper.showSuccToast('删除成功');
+				});
+			} catch (e) {
+				console.log(e);
+			}
+		}
+		pageHelper.showConfirm('确认删除？删除不可恢复', callback);
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
+	},
 
-    },
+	bindStatusTap: async function (e) {
+		if (!AdminBiz.isAdmin(this)) return;
+		let id = pageHelper.dataset(e, 'id');
+		let status = pageHelper.dataset(e, 'status');
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
+		let params = {
+			id,
+			status
+		}
+		try {
+			await cloudHelper.callCloudSumbit('admin/user_status', params).then(res => {
+				pageHelper.modifyListNode(id, this.data.dataList.list, 'USER_STATUS', status, 'USER_MINI_OPENID');
+				this.setData({
+					dataList: this.data.dataList
+				});
+				pageHelper.showSuccToast('设置成功');
+			});
+		} catch (e) {
+			console.log(e);
+		}
+	},
 
-    },
+	_getSearchMenu: async function () {
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
+		let sortItems = [];
+		let sortMenus = [{
+				label: '全部',
+				type: '',
+				value: ''
+			}, {
+				label: '正常',
+				type: 'status',
+				value: 1
+			}, 
+			{
+				label: '注册时间正序',
+				type: 'sort',
+				value: 'newasc'
+			},
+			{
+				label: '注册时间倒序',
+				type: 'sort',
+				value: 'newdesc'
+			},
 
-    },
+		]
+		this.setData({
+			sortItems,
+			sortMenus
+		})
 
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
 
-    },
+	}
 
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
 })
