8763d5596941ea63c3cdfb4e0361258516ee51fb
moshangongzi <2061935864@qq.com>
2022-06-22 19:56:00 +0800

    完成参加活动

--- projects/meet/detail/meet_detail.js
+++ projects/meet/detail/meet_detail.js
@@ -28,17 +28,6 @@ Page({
 			url: `../join/meet_join?id=${e.currentTarget.dataset.id}`,
 		})
 		console.log(e.currentTarget.dataset.id);
-		// db.collection('check_table').add({
-		// 	// data 字段表示需新增的 JSON 数据
-		// 	data: {
-		// 	  // _id: 'todo-identifiant-aleatoire', // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
-		// 	  act_id: e.currentTarget.dataset.id,
-		// 	  user_id: 1
-		// 	},
-		// 	success: res => {
-		// 	  // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
-		// 	  console.log(res)
-		// 	}
-		//   })
+		
 	}
 })
--- projects/meet/join/meet_join.js
+++ projects/meet/join/meet_join.js
@@ -1,16 +1,20 @@
-// let behavior = require('../../../behavior/meet_join_bh.js');
-// let PassortBiz = require('../../../biz/passport_biz.js');
-// let skin = require('../../skin/skin.js');
 const db = wx.cloud.database()
 Page({
-	// behaviors: [behavior], 
-
+	data: {
+		id: '',
+		info: {
+			name: '',
+			phone: '',
+			studentID: '',
+			Class: ''
+		},
+	},
 	onLoad: function (option) {
-		// PassortBiz.initPage({
-		// 	skin,
-		// 	that: this
-		// });
-
+		// console.log(option.id);
+		this.setData({
+			'info.act_id': option.id,
+			id: option.id
+		})
 		db.collection('ax_meet').where({
 			_id: option.id
 		}).get({
@@ -23,6 +27,64 @@ Page({
 				})
 				console.log(this.data.meet);
 			}
+		})
+	},
+	nameInput(e) {
+		console.log(e);
+		this.setData({
+			'info.name': e.detail
+		})
+	},
+	phoneInput(e) {
+		this.setData({
+			'info.phone': e.detail
+		})
+	},
+	studentIDInput(e) {
+		this.setData({
+			'info.studentID': e.detail
+		})
+	},
+	ClassInput(e) {
+		this.setData({
+			'info.Class': e.detail
+		})
+	},
+	save(e) {
+		if(this.data.info.name===''||this.data.info.phone===''||this.data.info.studentID===''||this.data.info.Class===''){
+			wx.showToast({
+				icon: 'none',
+				title: '必填项不能为空',
+			})
+			return 
+		}
+		if(this.data.info.phone.length!==11){
+			wx.showToast({
+				icon: 'none',
+				title: '请输入正确的手机号',
+			})
+			return 
+		}
+		if(this.data.info.studentID.length!==9){
+			wx.showToast({
+				icon: 'none',
+				title: '请输入正确的学号',
+			})
+			return 
+		}
+		db.collection('check_table').add({
+			// data 字段表示需新增的 JSON 数据
+			data: this.data.info,
+			success: res => {
+				// res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+				console.log(res)
+				wx.showToast({
+					title: '已提交审核',
+				})
+			}
+		})
+		wx.navigateTo({
+			url: `../detail/meet_detail?id=${this.data.id}`,
 		})
 	},
 })
