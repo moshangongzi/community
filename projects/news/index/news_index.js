359cba30bad43ed5c0ecc28b84abf5705c19dfd5
moshangongzi <2061935864@qq.com>
2022-06-24 09:49:46 +0800

    修改了数据

--- projects/news/index/news_index.js
+++ projects/news/index/news_index.js
@@ -4,8 +4,22 @@ let skin = require('../../skin/skin.js');
 const db = wx.cloud.database()
 Page({
 	behaviors: [behavior],
+	data: {
+		id: '',
+		dataList: {}
+	},
 
-	onReady: function () {
+	onLoad(options) {
+		console.log('onLoad',options.id);
+		this.setData({
+			id: options.id,
+			_params:options.id
+		})
+	},
+
+	onShow: function () {
+		let id1 = this.data.id
+		console.log('onShow',this.data.id);
 		PassortBiz.initPage({
 			skin,
 			that: this,
@@ -15,15 +29,20 @@ Page({
 		});
 
 		this._setCateTitle(skin);
-		db.collection('com_notice').get({
+		console.log(id1);
+		db.collection('com_notice').where({
+			kind: parseInt(this.data.id)
+		}).get({
 			success: res => {
-			  // res.data 是一个包含集合中有权限访问的所有记录的数据，不超过 20 条
-			//   console.log("fff",res) 
-			 this.setData({
-				'dataList.list':res.data,
-				'dataList.isLoad': true
-			 })
+				console.log(this.data.id);
+				console.log(res.data);
+				// res.data 是一个包含集合中有权限访问的所有记录的数据，不超过 20 条
+				//   console.log("fff",res) 
+				this.setData({
+					'dataList.list': res.data,
+					'dataList.isLoad': true
+				})
 			}
-		  })
+		})
 	},
 })
