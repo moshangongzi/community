dcbe812e672bc8ee920a4a0377d9b3a2280a977c
Amy <2503360709@qq.com>
2022-06-24 09:50:11 +0800

    完成了我的活动

--- projects/my/join/my_join.js
+++ projects/my/join/my_join.js
@@ -1,17 +1,85 @@
-let behavior = require('../../../behavior/my_join_bh.js');
-let PassortBiz = require('../../../biz/passport_biz.js');
-let skin = require('../../skin/skin.js');
-
+// const db = wx.cloud.database().collection('activeList')
+const db = wx.cloud.database()
 Page({
-	behaviors: [behavior], 
-
-	onReady: function () {
-		PassortBiz.initPage({
-			skin,
-			that: this,
-			isLoadSkin: true,
+	// behaviors: [behavior], 
+	data: {
+		inputValue: '',
+		actList: [],
+		actIdList: [],
+		isLoad: false,
+		openid: '',
+	},
+	onLoad() {
+		this.getopenid();
+	},
+	//获取openid
+	getopenid() {
+		wx.cloud.callFunction({
+			name: 'getOpenid'
+		}).then(res => {
+			this.setData({ openid: res.result.openid });
+			this.getActIdList();
+			console.log('获取openid函数成功', res.result.openid);
+		}).catch(res => {
+			console.log('获取openid函数失败', res)
 		});
-
-		this.getSearchMenu(skin, this);
+	},
+	// 获取用户的活动id列表
+	getActIdList() {
+		db.collection('User').doc(this.data.openid)
+			.get()
+			.then(res => {
+				this.setData({
+					actIdList: res.data.actList
+				})
+				console.log('拿到了id列表', res.data.actList);
+				this.getActList()
+			})
+			.catch(err => {
+				console.log('没拿到id列表', err)
+			})
+	},
+	//拿到活动列表
+	getActList() {
+		let act = {};
+		let list = []
+		for (var i = 0; i < this.data.actIdList.length; i++) {
+			db.collection('activeList')
+				.doc(this.data.actIdList[i])
+				.get()
+				.then(res => {
+					act = res.data
+					list.push(act);
+					this.setData({
+						actList: list
+					})
+				})
+				.catch(res => {
+					console.log('获取用户的活动列表失败', res)
+				})
+		}
+	},
+	onChange(e) {
+		this.setData({
+			inputValue: e.detail,
+		});
+	},
+	//搜索筛选
+	onSearch() {
+		var value = this.data.inputValue;
+		db.where({
+			title: value
+		})
+			.get()
+			.then(res => {
+				this.setData({
+					actList: res.data,
+					inputValue: ''
+				})
+			})
+			.catch(err => {
+				console.log('筛选活动后数据请求失败', err)
+			})
+		console.log(this.data.inputValue)
 	},
 })
